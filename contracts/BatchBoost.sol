bd051cbb386150dd5a56bcfec74f11673273dd5f
Vivecboots <Blairwin05@gmail.com>
2023-05-11 18:50:25 -0700

    Updated and compiled Solidity contracts

--- contracts/BatchBoost.sol
+++ contracts/BatchBoost.sol
@@ -1,23 +1,21 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract BatchBoost {
-    address public USDT_TOKEN_ADDRESS;
-    address public TARGET_TOKEN_ADDRESS;
-
     IERC20 public usdtToken;
     IERC20 public targetToken;
-
+    AggregatorV3Interface internal priceFeed;
+    
     mapping(address => uint256) public deposits;
     uint256 public totalDeposits;
 
-    constructor(address _usdtTokenAddress, address _targetTokenAddress) {
-        USDT_TOKEN_ADDRESS = _usdtTokenAddress;
-        TARGET_TOKEN_ADDRESS = _targetTokenAddress;
-
-        usdtToken = IERC20(USDT_TOKEN_ADDRESS);
-        targetToken = IERC20(TARGET_TOKEN_ADDRESS);
+    constructor(address _usdtTokenAddress, address _targetTokenAddress, address _priceFeedAddress) {
+        usdtToken = IERC20(_usdtTokenAddress);
+        targetToken = IERC20(_targetTokenAddress);
+        priceFeed = AggregatorV3Interface(_priceFeedAddress);
     }
 
     function deposit(uint256 amount) external {
@@ -25,6 +23,15 @@ contract BatchBoost {
         usdtToken.transferFrom(msg.sender, address(this), amount);
         deposits[msg.sender] += amount;
         totalDeposits += amount;
+    }
+    
+    function executeBuy(uint256 maxPrice, uint256 minNonce) external {
+        // TODO: Implement the logic for executing buy opportunities using 1inch Aggregator API
+    }
+    
+    function getLatestPrice() public view returns (int) {
+        (,int price,,,) = priceFeed.latestRoundData();
+        return price;
     }
 
     // Additional functions for purchasing, distribution, and withdrawal will be added here
